d35ddc28943dc06f8166ec6f8f9ee7e987f3e0a0
TranHuy2k2 <giahuytran2002@gmail.com>
2022-06-13 02:17:46 +0700

    useStorage and ProgressBar

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import ProgressBar from './ProgressBar'
 function UploadForm(){
     const [file, setFile] = useState(null);
     const [error, setError] = useState(null);
@@ -18,10 +19,14 @@ function UploadForm(){
     }
     return (
         <form>
-            <input type="file" onChange={changeHandler}></input>
+            <label>
+                <input type="file" onChange={changeHandler}></input>
+                <span>+</span>
+            </label>
             <div className='output'>
                 {error && <div className='error'>{error}</div>}
                 {file && <div>{file.name}</div>}
+                {file && <ProgressBar file={file} setFile={setFile}/>}
             </div>
         </form>
     )
